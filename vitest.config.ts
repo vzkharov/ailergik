6c2df610e923602d3ff8cccb505b5be392191f23
vadzim.l <vadzim.l@realmond.com>
2024-10-31 21:37:00 +0300

    add: vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,31 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react()],
+  // set mode to test
+  mode: 'test',
+  test: {
+    name: '@ailergik/www',
+    // imitate browser environment
+    environment: 'jsdom',
+    // the reason - a lot of files in the src/ directory
+    exclude: [
+      'src/**/*.tsx',
+      'src/**/*.ts',
+      'node_modules/**/*.tsx',
+      'node_modules/**/*.ts',
+    ],
+    // include only test files
+    include: ['__tests__/**/*.spec.ts'],
+
+    // alias support
+    alias: {
+      '~/': new URL('src/', import.meta.url).pathname,
+    },
+    // experimental feature - if it breaks, just remove it
+    typecheck: {
+      enabled: true,
+    },
+  },
+});
