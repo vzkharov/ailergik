c6811889bb869369faec4748efd4e9e465f00258
vadzim.l <vadzim.l@realmond.com>
2024-11-07 02:09:57 +0300

    update: logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,25 +3,18 @@ import Link from 'next/link'
 import { cn } from '~/lib/utils'
 import type { StyleProps } from '~/lib/types'
 
-import { Image } from '~/components/ui/image'
-
 const Logo = ({ className, ...props }: StyleProps) => (
   <Link
     {...props}
-    href="/"
+    href="/partners"
     className={cn(
       'relative block h-fit w-max flex-none select-none',
       className,
     )}
   >
-    <Image
-      width={300}
-      height={100}
-      alt="logo"
-      loading="eager"
-      src="/logo.png"
-      className="aspect-[3/1] h-9 w-auto object-contain lg:h-11"
-    />
+    <h2 className="translate-y-0.5 font-helveticaBold text-xl uppercase">
+      AiLergik
+    </h2>
   </Link>
 )
 
